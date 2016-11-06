92e2a8edd0057d85dacf75ef459562f2f08b476e
Evgeny Boxer <evgeny.boxer@gmail.com>
2016-11-06 11:24:01 +1100

    Support React 0.15+ / ES2015 / Add mandatory API_KEY / Use little-loader instead of scriptjs

--- example/src/example.js
+++ example/src/example.js
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var SelectGooglePlaces = require('react-select-google-places');
 
+const API_KEY = '<YOUR-API-KEY>';
+
 var App = React.createClass({
   onChange (value) {
     console.log(value);
@@ -10,9 +12,9 @@ var App = React.createClass({
 		return (
 			<div>
         <h2>Demo Single selection</h2>
-        <SelectGooglePlaces optionsForSelect={{multi: false, cache: false}} initialValue={{name: 'New York'}} onChange={this.onChange} />
+        <SelectGooglePlaces apiKey={API_KEY} optionsForSelect={{multi: false, cache: false}} initialValue={{label: 'New York', value: 'New York'}} onChange={this.onChange} />
         <h2>Demo Multiple selection</h2>
-        <SelectGooglePlaces optionsForSelect={{multi: true, cache: false }} initialValue={[{name: 'New York'}, {name: 'Chicago'}]} onChange={this.onChange} />
+        <SelectGooglePlaces apiKey={API_KEY} optionsForSelect={{multi: true, cache: false }} initialValue={[{label: 'New York', value: 'New York'}, {label: 'Chicago', value: 'Chicago'}]} onChange={this.onChange} />
 			</div>
 		);
 	}
--- src/SelectGooglePlaces.js
+++ src/SelectGooglePlaces.js
@@ -1,73 +1,48 @@
-var React = require('react');
-
+import React from 'react';
 import Select from 'react-select';
-import scriptjs from 'scriptjs';
+import load from 'little-loader';
 
-const SelectGooglePlaces = React.createClass({
-  displayName: 'SelectGooglePlaces',
-
-  propTypes: {
-    multi: React.PropTypes.bool,              // Single or multiple places selection
-    language: React.PropTypes.string,         // Language code for loading Google Maps API
-    country: React.PropTypes.string,          // ISO 3166-1 Alpha-2 country code for limiting results
-    types: React.PropTypes.array,             // Types of results to be displayed
-                                              // See https://developers.google.com/places/supported_types#table3
-    formatName: React.PropTypes.func,         // Receives the result placesService.getDetails() and returns a formatted name
-                                              // See https://developers.google.com/maps/documentation/javascript/3.exp/reference#PlaceResult
-    onChange: React.PropTypes.func,           // onChange handler: function (newValue) {}
-    optionsForSelect: React.PropTypes.object, // See https://github.com/JedWatson/react-select#further-options
-    initialValue: React.PropTypes.any
-  },
-
-  getDefaultProps () {
-    return {
-      language: 'en',
-      country: 'USA',
-      types: ['(cities)'],
-      formatName(placeResult) {
-        return placeResult.address_components[0].long_name;
-      },
-      optionsForSelect: {
-        multi: true,
-        cache: false,
-        name: 'places'
-      },
-      onChange: function(value) {},
-      initialValue: null
-    };
-  },
-
-  getInitialState () {
-    return {
+class SelectGooglePlaces extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getPredictions = this.getPredictions.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.state = {
       value: this.props.initialValue,
       autocompleteService: null,
-      placesService: null
+      placesService: null,
+      googleMapsLoading: false,
     };
-  },
+  }
 
-  componentDidMount() {
+  componentWillMount() {
     if (typeof window.google === 'undefined') {
-      window.googleMapsLoaded = () => {
-        scriptjs.done('google-maps-places');
-      };
-
-      scriptjs(`https://maps.googleapis.com/maps/api/js?libraries=places&language=${this.props.language}`, 'google-maps-places');
-      scriptjs.ready('google-maps-places', () => {
-        this.handleLoaded(google.maps);
-      });
+      const { language, apiKey } = this.props;
+      if (window.googleMapsLoading) {
+        this.waitForGoogleAPI();
+      } else {
+        window.googleMapsLoading = true; 
+        load(`https://maps.googleapis.com/maps/api/js?libraries=places&language=${language}&key=${apiKey}`, (err) => {
+          if (!err) {
+            window.googleMapsLoading = false;
+            this.handleLoaded(window.google.maps);
+          }
+        });
+      }
     } else {
-      this.handleLoaded(google.maps);
+      this.handleLoaded(window.google.maps);
     }
-  },
+  }
 
-  handleLoaded(googleMaps) {
-    this.setState({ 
-      autocompleteService: new googleMaps.places.AutocompleteService(),
-      placesService: new googleMaps.places.PlacesService(this.refs.attributions)
-    });
-  },
+  waitForGoogleAPI() {
+    setTimeout(() => {
+      if (!window.googleMapsLoading) {
+        this.handleLoaded(window.google.maps);
+      }
+    }, 300);
+  }
 
-  onChange (value) {
+  onChange(value) {
     let selectedPlace;
     if (value && value.constructor === Array) {
       selectedPlace = value[value.length - 1];
@@ -78,61 +53,95 @@ const SelectGooglePlaces = React.createClass({
     // Once a place is selected, fetch more data for it (long_name, lat, lng, etc.)
     if (selectedPlace) {
       this.processPlace(selectedPlace, () => {
-        this.setState({ value: value });
+        this.setState({ value });
         if (this.props.onChange) {
           this.props.onChange(value);
         }
       });
-      
     } else {
-      this.setState({ value: value });
+      this.setState({ value });
       this.props.onChange(value);
     }
-  },
+  }
 
-  processPlace (autocompletePrediction, callback) {
+  getPredictions(input, callback) {
+    if (this.state.autocompleteService && input) {
+      const geocoderRequest = {
+        input,
+        types: this.props.types,
+        componentRestrictions: {
+          country: this.props.country,
+        },
+      };
+      this.state.autocompleteService.getPlacePredictions(geocoderRequest, (data) => {
+        // Copy description into the name attribute
+        if (data) {
+          data.map((result) => {
+            result.label = result.description;
+            result.value = result.description;
+          });
+        }
+        callback(null, { options: data, complete: false });
+      });
+    } else {
+      callback(null, { options: [], complete: false });
+    }
+  }
+
+  processPlace(autocompletePrediction, callback) {
     if (!autocompletePrediction.place_id) {
       callback();
       return;
     }
-    this.state.placesService.getDetails({placeId: autocompletePrediction.place_id}, (placeResult) => {
+    this.state.placesService.getDetails({ placeId: autocompletePrediction.place_id }, (placeResult) => {
       autocompletePrediction = Object.assign(autocompletePrediction, placeResult);
       autocompletePrediction.name = this.props.formatName(placeResult);
       callback();
     });
-  },
+  }
 
-  getPredictions (input, callback) {
-    if (this.state.autocompleteService && input) {
-      const geocoderRequest = { 
-        input: input, 
-        types: this.props.types,
-        componentRestrictions: {
-          country: this.props.country
-        }
-      };
-      this.state.autocompleteService.getPlacePredictions(geocoderRequest, function(data){
-        // Copy description into the name attribute
-        if (data) {
-          data.map(function(result) {
-            result.name = result.description;
-          });  
-        }
-        callback(null, {options: data, complete: false});
-      });  
-    }
-    callback(null, {options: [], complete: false});
-  },
+  handleLoaded(googleMaps) {
+    this.setState({
+      autocompleteService: new googleMaps.places.AutocompleteService(),
+      placesService: new googleMaps.places.PlacesService(this.attributionsEl),
+    });
+  }
 
-  render () {
+  render() {
     return (
-      <div>
-        <Select.Async value={this.state.value} valueKey="name" labelKey="name" loadOptions={this.getPredictions} onChange={this.onChange} {...this.props.optionsForSelect} />
-        <div ref="attributions"></div>
-      </div>
+      <span>
+        <Select.Async value={this.state.value} loadOptions={this.getPredictions} onChange={this.onChange} {...this.props.optionsForSelect} />
+        <div ref={(el) => this.attributionsEl = el}></div>
+      </span>
     );
   }
-});
+}
 
+SelectGooglePlaces.propTypes = {
+  apiKey: React.PropTypes.string, // API Key
+  language: React.PropTypes.string,          // Language code for loading Google Maps API
+  country: React.PropTypes.string,           // ISO 3166-1 Alpha-2 country code for limiting results
+  types: React.PropTypes.array,              // Types of results to be displayed
+                                             // See https://developers.google.com/places/supported_types#table3
+  formatName: React.PropTypes.func,          // Receives the result placesService.getDetails() and returns a formatted name
+                                             // See https://developers.google.com/maps/documentation/javascript/3.exp/reference#PlaceResult
+  onChange: React.PropTypes.func,            // onChange handler: function (newValue) {}
+  optionsForSelect: React.PropTypes.object,  // See https://github.com/JedWatson/react-select#further-options
+  initialValue: React.PropTypes.any,
+};
+
+SelectGooglePlaces.defaultProps = {
+  language: 'en',
+  country: 'USA',
+  types: ['(cities)'],
+  formatName: (placeResult) => placeResult.address_components[0].long_name,
+  optionsForSelect: {
+    multi: false,
+    cache: false,
+    name: 'places',
+  },
+  onChange: () => {},
+  initialValue: null,
+};
 
 export default SelectGooglePlaces;
